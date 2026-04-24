38da5dd192ca00b27e9aceebc342ef5c18e194fc
shadiaal <shadiaalmtiri@gmail.com>
2026-04-24 21:53:16 +0300

    3 edit

--- src/app/services/start.service.ts
+++ src/app/services/start.service.ts
@@ -6,14 +6,12 @@ import { environment } from '../../environments/environment';
 })
 export class StartService {
 
-  /*  private apiUrl = 'http://localhost:5286/api/recommend';*/
+
   private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
-  //sendMessage(data: any) {
-  //  return this.http.post(this.apiUrl, data);
-  //}
+
   sendMessage(data: any) {
     return this.http.post(`${this.baseUrl}/api/recommend`, data);
   }
