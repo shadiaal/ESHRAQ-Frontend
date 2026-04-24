29cc2154a0e76f1f07ca8e0757d4b380472d7aef
shadiaal <shadiaalmtiri@gmail.com>
2026-04-24 14:32:13 +0300

    enable Frontend in production

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  private apiUrl = 'http://localhost:5286/api/contact';
-
+ /* private apiUrl = 'http://localhost:5286/api/contact';*/
+  private baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
+  //sendMessage(data: any) {
+  //  return this.http.post(this.apiUrl, data);
+  //}
   sendMessage(data: any) {
-    return this.http.post(this.apiUrl, data);
+    return this.http.post(`${this.baseUrl}/api/contact`, data);
   }
-
 }
 
 
--- src/app/services/start.service.ts
+++ src/app/services/start.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class StartService {
 
-  private apiUrl = 'http://localhost:5286/api/recommend';
+  /*  private apiUrl = 'http://localhost:5286/api/recommend';*/
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
+  //sendMessage(data: any) {
+  //  return this.http.post(this.apiUrl, data);
+  //}
   sendMessage(data: any) {
-    return this.http.post(this.apiUrl, data);
+    return this.http.post(`${this.baseUrl}/api/recommend`, data);
   }
 }
 
